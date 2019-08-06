3fd0fe23dac1c4823171b75f84c0068246336eac
zhuxiang <892366102@qq.com>
2019-08-07 00:18:28 +0800

    生成项目基础提交:DictVal.java

---
+++ src/main/java/com/zgb/test/utils/aspect/annotation/DictVal.java
@@ -0,0 +1,41 @@
+/*
+ *    Copyright (c) 2018-2025, Nanjing Zhengebang Software Corp.,Ltd All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lsyzx (zhux@zhengebang.com)
+ */
+package com.zgb.test.utils.aspect.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+/**
+ * @author: lsyzx (zhux@zhengebang.com)
+ * 创建时间: 2019-08-07 00:18:27
+ * 描述: 字典aop类
+ * 历史修改:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DictVal {
+
+    /**
+     * 数据code
+     * @return
+     */
+    String dicCode();
+
+}
