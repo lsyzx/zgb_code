d44af5bcd3b5a750c20225a65d5db89a9fd5ae62
zhuxiang <892366102@qq.com>
2019-08-07 00:18:29 +0800

    生成项目基础提交:SysRoleUserMapper.java

---
+++ src/main/java/com/zgb/test/sys/mapper/SysRoleUserMapper.java
@@ -0,0 +1,46 @@
+/*
+ *    Copyright (c) 2018-2025, Nanjing Zhengebang Software Corp.,Ltd All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lsyzx (zhux@zhengebang.com)
+ */
+package com.zgb.test.sys.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zgb.test.sys.entity.SysRoleUser;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+/**
+ * @author: lsyzx (zhux@zhengebang.com)
+ * 创建时间: 2019-08-07 00:18:27
+ * 描述: 用户角色关系
+ * 历史修改:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+public interface SysRoleUserMapper extends BaseMapper<SysRoleUser> {
+
+    /**
+     * 批量插入
+     * @param list
+     */
+    void batchInsert(@Param("list") List<SysRoleUser> list);
+
+    /**
+     * 查询用户选中的角色id
+     * @param userId
+     * @return
+     */
+    List<Long> getRoleIdsByUserId(@Param("userId") Long userId);
+}
