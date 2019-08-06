f770aa6e2808a69d99c0c95916298cb631c063fb
zhuxiang <892366102@qq.com>
2019-08-07 00:18:29 +0800

    生成项目基础提交:SysRoleMenuServiceImpl.java

---
+++ src/main/java/com/zgb/test/sys/service/impl/SysRoleMenuServiceImpl.java
@@ -0,0 +1,36 @@
+/*
+ *    Copyright (c) 2018-2025, Nanjing Zhengebang Software Corp.,Ltd All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lsyzx (zhux@zhengebang.com)
+ */
+package com.zgb.test.sys.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zgb.test.sys.entity.SysRoleMenu;
+import com.zgb.test.sys.mapper.SysRoleMenuMapper;
+import com.zgb.test.sys.service.SysRoleMenuService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: lsyzx (zhux@zhengebang.com)
+ * 创建时间: 2019-08-07 00:18:27
+ * 描述: 角色菜单关系
+ * 历史修改:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+@Service
+public class SysRoleMenuServiceImpl extends ServiceImpl<SysRoleMenuMapper, SysRoleMenu> implements SysRoleMenuService {
+
+}
