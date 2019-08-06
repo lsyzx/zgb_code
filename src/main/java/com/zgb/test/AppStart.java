c25bfb50d1b533476d1c7cd3697c3e3c41256a9f
zhuxiang <892366102@qq.com>
2019-08-07 00:18:29 +0800

    生成项目基础提交:AppStart.java

---
+++ src/main/java/com/zgb/test/AppStart.java
@@ -0,0 +1,65 @@
+/*
+ *    Copyright (c) 2018-2025, Nanjing Zhengebang Software Corp.,Ltd All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lsyzx (zhux@zhengebang.com)
+ */
+package com.zgb.test;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author: lsyzx (zhux@zhengebang.com)
+ * 创建时间: 2019-08-07 00:18:27
+ * 描述: 启动项
+ * 历史修改:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+@MapperScan("com.zgb.test.*.mapper")
+@EnableTransactionManagement
+@SpringBootApplication
+public class AppStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class, args);
+    }
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 处理vue 404
+     * @return
+     */
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+        return factory -> {
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+            factory.addErrorPages(error404Page);
+        };
+    }
+}
