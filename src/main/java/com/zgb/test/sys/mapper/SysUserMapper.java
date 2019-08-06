3dfe18dab53bafc7cf26917acc3f09cb7a0d1e0b
zhuxiang <892366102@qq.com>
2019-08-07 00:18:27 +0800

    生成项目基础提交:SysUserMapper.java

---
+++ src/main/java/com/zgb/test/sys/mapper/SysUserMapper.java
@@ -0,0 +1,42 @@
+/*
+ *    Copyright (c) 2018-2025, Nanjing Zhengebang Software Corp.,Ltd All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lsyzx (zhux@zhengebang.com)
+ */
+package com.zgb.test.sys.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.zgb.test.sys.entity.SysUser;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+/**
+ * @author: lsyzx (zhux@zhengebang.com)
+ * 创建时间: 2019-08-07 00:18:27
+ * 描述: 用户信息
+ * 历史修改:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+public interface SysUserMapper extends BaseMapper<SysUser> {
+
+    /**
+    * 查询用户分页信息
+    * @param page
+    * @param sysUser
+    * @return
+    */
+    List<SysUser> queryUserPageInfo(Page page, @Param("sysUser") SysUser sysUser);
+}
