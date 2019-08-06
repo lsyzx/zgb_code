c897b599d421a3fbc4d89f69bc7a6e02f4093045
zhuxiang <892366102@qq.com>
2019-08-07 00:18:28 +0800

    生成项目基础提交:CommonApi.java

---
+++ src/main/java/com/zgb/test/common/api/CommonApi.java
@@ -0,0 +1,51 @@
+/*
+ *    Copyright (c) 2018-2025, Nanjing Zhengebang Software Corp.,Ltd All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lsyzx (zhux@zhengebang.com)
+ */
+package com.zgb.test.common.api;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.zgb.test.utils.em.CommonEnum;
+import com.zgb.test.utils.result.ApiResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * @author: lsyzx (zhux@zhengebang.com)
+ * 创建时间: 2019-08-07 00:18:27
+ * 描述: 公共接口
+ * 历史修改:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/common/api", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@Api(tags = "公共接口")
+public class CommonApi {
+
+}
