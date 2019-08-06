7eea7c6f5ac1fd9e9afad825fbae1c32f64cf490
zhuxiang <892366102@qq.com>
2019-08-07 00:18:29 +0800

    生成项目基础提交:FilterProperties.java

---
+++ src/main/java/com/zgb/test/utils/config/FilterProperties.java
@@ -0,0 +1,42 @@
+/*
+ *    Copyright (c) 2018-2025, Nanjing Zhengebang Software Corp.,Ltd All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lsyzx (zhux@zhengebang.com)
+ */
+package com.zgb.test.utils.config;
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author: lsyzx (zhux@zhengebang.com)
+ * 创建时间: 2019-08-07 00:18:27
+ * 描述: 配置过滤器配置
+ * 历史修改:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+@Data
+@ConfigurationProperties(prefix = "auth.url")
+public class FilterProperties {
+
+    /**
+     * 需要授权的系统接口地址
+     */
+    private String sysUrls;
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this);
+    }
+}
