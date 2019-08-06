d87d2f92c4afcf70fbbe5d54762fe602f47c4e60
zhuxiang <892366102@qq.com>
2019-08-07 00:18:29 +0800

    生成项目基础提交:UserDataResult.java

---
+++ src/main/java/com/zgb/test/utils/result/UserDataResult.java
@@ -0,0 +1,55 @@
+/*
+ *    Copyright (c) 2018-2025, Nanjing Zhengebang Software Corp.,Ltd All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lsyzx (zhux@zhengebang.com)
+ */
+package com.zgb.test.utils.result;
+
+import com.zgb.test.sys.entity.SysMenu;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author: lsyzx (zhux@zhengebang.com)
+ * 创建时间: 2019-08-07 00:18:27
+ * 描述: 用户信息响应对象
+ * 历史修改:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDataResult implements Serializable {
+
+  /**
+   * 用户信息
+   */
+  private Object sysUser;
+
+  /**
+   * 菜单信息
+   */
+  private List<SysMenu> menus;
+
+  /**
+   * 权限信息
+   */
+  private List<SysMenu>  permissions;
+
+}
